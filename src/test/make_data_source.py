034eed71f18cc3642b5c7fcc9c5a00324650d352
Dandoleiro <62514735+Dandoleiro@users.noreply.github.com>
2021-05-12 19:47:15 +0200

    Now it works  (better) pt2

--- src/test/make_data_source.py
+++ src/test/make_data_source.py
@@ -1,46 +1,32 @@
 import argparse
 import csv
-import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-dataset', required=True)
-
 opt = parser.parse_args()
-
 dataset = opt.dataset
-
-
-path = "../../data/%s" % (dataset)
 
 def readcsv(filename):
     with open(filename, encoding="ISO-8859-1") as f:
         reader = csv.reader(f)
         return list(reader)
 
-source1 = readcsv("../../data/%s/tableA.csv" % dataset)[1:]
-source2 = readcsv("../../data/%s/tableB.csv" % dataset)[1:]
-
-def functionSource(source, num):
+def functionSource(source):
 
     exp=""
-    txt=""
-    experimental= ""
-    for source in source1:
-        source.remove(source[0])
-        experimental =str(source)
-        experimental = experimental.replace("[","")
-        experimental = experimental.replace("]","")
-        experimental = experimental.replace("'","")
-        exp += experimental+"\n"
+    for s in source:
+        s.remove(s[0])
+        for i in range(len(s)):
+            exp += (str(s[i]))+" "
+            
+        exp+="\n"
+    return exp
 
-        print(experimental)
-    if(num == 1):
-        with open("../../data/%s/source_1.txt" % (dataset), "w+", encoding="utf-8") as f:
-            f.write(exp)
-    else:
-        with open("../../data/%s/source_2.txt" % (dataset), "w+", encoding="utf-8") as f:
-            f.write(exp)
+source1=functionSource(readcsv("../../data/%s/tableA.csv" % dataset)[1:])
+source2=functionSource(readcsv("../../data/%s/tableB.csv" % dataset)[1:])
 
+with open("../../data/%s/source_1.txt" % (dataset), "w+", encoding="utf-8") as f:
+    f.write(source1)
 
-functionSource(source1,1)
-functionSource(source2,2)
+with open("../../data/%s/source_2.txt" % (dataset), "w+", encoding="utf-8") as f:
+    f.write(source2)
