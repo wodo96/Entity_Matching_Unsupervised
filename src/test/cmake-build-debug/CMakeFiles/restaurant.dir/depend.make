139a6f1608bfc41f3bb484dfb100ade93dd973f4
wodo96 <renzo.cuomo1996@gmail.com>
2021-06-19 17:38:04 +0200

    -Removed old things -Added output of prerunned datasets

--- src/test/cmake-build-debug/CMakeFiles/restaurant.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for restaurant.
-# This may be replaced when dependencies are built.
--- src/test/compile.sh
+++
@@ -1,3 +0,0 @@
-g++ -fopenmp restaurant_matrix.cpp -o restaurant_matrix
-g++ -fopenmp product_matrix.cpp -o product_matrix
-g++ -fopenmp paper_matrix.cpp -o paper_matrix
